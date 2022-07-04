c12cb38b1cc6aff3d2e99f9f5e87f335abf1855b
yung78 <alexdrum@mail.ru>
2022-07-04 17:53:15 +0300

    Add dirty_main

---
+++ dirty_main.py
@@ -0,0 +1,7 @@
+import main
+from main import *
+
+
+if __name__ == '__main__':
+    main.get_employees()
+    main.calculate_salary()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from application.salary import calculate_salary
 from application.people import get_employees
+import datetime
 
 
 if __name__ == '__main__':
     get_employees()
     calculate_salary()
+    print(datetime.datetime.now())
